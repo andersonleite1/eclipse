df539f6b3025ada7f48ca9a357636b728528f34e
Anderson S. Leite <47832142+andersonslt@users.noreply.github.com>
2020-01-16 08:29:53 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/andersonleite/git/HelloGit.java
@@ -0,0 +1,14 @@
+package br.com.andersonleite.git;
+
+/**
+ * @author Anderson Leite
+ * Exemplo de integração do eclipse com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git");
+
+	}
+}
